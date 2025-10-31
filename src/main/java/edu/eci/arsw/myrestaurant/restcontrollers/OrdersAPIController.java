8a4c8ae8a0596e30fb7d8ede8d053dd77af02371
unknown <santiago.hurtado-m@labinfo.is.escuelaing.edu.co>
2025-10-31 07:54:30 -0500

    Primer punto y avance de bono

--- src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
+++ src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
@@ -16,6 +16,7 @@
  */
 package edu.eci.arsw.myrestaurant.restcontrollers;
 
+import edu.eci.arsw.myrestaurant.RestapidemoApplication;
 import edu.eci.arsw.myrestaurant.model.Order;
 import edu.eci.arsw.myrestaurant.model.ProductType;
 import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
@@ -23,18 +24,27 @@ import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
  * @author hcadavid
  */
+@RestController
+@RequestMapping("/orders")
 public class OrdersAPIController {
+
+    private RestaurantOrderServicesStub restaurantOrderServicesStub;
+
+    @Autowired
+    public OrdersAPIController(RestaurantOrderServicesStub restaurantOrderServicesStub){
+        this.restaurantOrderServicesStub = restaurantOrderServicesStub;
+    }
+
 
     
 }
